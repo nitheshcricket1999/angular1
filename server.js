cdcd3b891865432bf6e38226ceaabef27672ce4d
unknown <nithucricket1999@gmail.com>
2021-03-05 17:48:16 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/healthapp1'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/healthapp1/index.html'));
+res.sendFile(path.join(__dirname+'/dist/healthapp1 /index.html'));
 });
 
 // Start the app by listening on the default Heroku port
